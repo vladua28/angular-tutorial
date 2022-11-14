0680fbd70b4c1ce005541255394988991c1a07b6
vladua28 <78270204+vladua28@users.noreply.github.com>
2022-11-14 20:27:53 +0200

    2 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
+  products = [
+    {
+      name: 'Iphone 13 pro max',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, ab?',
+    },
+    {
+      name: 'Phone Mini',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, ab?',
+    },
+    {
+      name: 'Phone Standart',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
